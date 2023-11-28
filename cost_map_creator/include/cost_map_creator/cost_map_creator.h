3c62dc6b68fa0cf7ee1f2b6de50279ef2ac341ce
Iwamura-Yuuka <ee203071@meiji.ac.jp>
2023-11-28 17:23:29 +0900

    pedsim_data and robot_odom can be subscribed

--- cost_map_creator/include/cost_map_creator/cost_map_creator.h
+++ cost_map_creator/include/cost_map_creator/cost_map_creator.h
@@ -4,9 +4,9 @@
 
 #include <ros/ros.h>
 #include <queue>
-#include <nav_msgs/OccupancyGrid.h>
 #include <geometry_msgs/PoseArray.h>
 #include <geometry_msgs/Pose.h>
+#include <nav_msgs/Odometry.h>
 
 #include <pedsim_msgs/AgentState.h>
 #include <pedsim_msgs/AgentStates.h>
@@ -21,6 +21,7 @@ public:
 private:
     // コールバック関数
     void pedestrian_data_callback(const pedsim_msgs::AgentStatesConstPtr& agents);
+    void robot_odom_callback(const nav_msgs::Odometry::ConstPtr& msg);
 
     // 引数あり関数
 
@@ -37,6 +38,7 @@ private:
     
     // msgの受け取り判定用
     bool flag_ped_states_ = false;
+    bool flag_robot_odom_ = false;
 
     // NodeHandle
     ros::NodeHandle nh_;
@@ -44,12 +46,14 @@ private:
 
     // Subscriber
     ros::Subscriber sub_ped_states_;
+    ros::Subscriber sub_robot_odom_;
 
     // Publisher
-    // ros::Publisher pub_local_map_;
+    ros::Publisher pub_current_ped_position_;
 
     std::queue<pedsim_msgs::AgentStatesConstPtr> ped_states_;  // 歩行者情報
-    pedestrian_msgs::PersonState current_state_;
+    nav_msgs::Odometry robot_odom_;
+    // pedestrian_msgs::PersonState current_state_;
     // pedsim_msgs::AgentStateConstPtr person_;
     // nav_msgs::OccupancyGrid local_map_;
     // geometry_msgs::PoseArray obs_poses_;
--- cost_map_creator/src/cost_map_creator.cpp
+++ cost_map_creator/src/cost_map_creator.cpp
@@ -7,34 +7,63 @@ CostMapCreator::CostMapCreator():private_nh_("~")
 
     // subscriber
     sub_ped_states_ = nh_.subscribe("/pedsim_simulator/simulated_agents", 1, &CostMapCreator::pedestrian_data_callback, this);
+    sub_robot_odom_ = nh_.subscribe("/pedsim_simulator/robot_position", 1, &CostMapCreator::robot_odom_callback, this);
 
     // publisher
+
+    // debug
+    pub_current_ped_position_ = nh_.advertise<geometry_msgs::PoseArray>("/current_ped_position", 1);
 
 }
 
 void CostMapCreator::pedestrian_data_callback(const pedsim_msgs::AgentStatesConstPtr& agents)
 {
     ped_states_.emplace(agents);
+    // ROS_INFO_STREAM("id[1]'s x is " << agents->agent_states[1].pose.position.x);  // デバック用
     flag_ped_states_ = true;
+}
+
+void CostMapCreator::robot_odom_callback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+    robot_odom_ = *msg;
+    ROS_INFO_STREAM("robot x is " << robot_odom_.pose.pose.position.x);  // デバック用
+    flag_robot_odom_ = true;
 }
 
 // デバック用
 void CostMapCreator::id_veiwer()
 {
+    geometry_msgs::PoseArray current_ped_poses;  // 歩行者の現在位置格納用
+    current_ped_poses.header.frame_id  = "odom";
+    geometry_msgs::Pose current_ped_pose;
+
+    pedestrian_msgs::PersonState current_state;
+    
     const auto people_states = ped_states_.front();
 
     for(const auto& person : people_states->agent_states)
     {
-        current_state_.id = person.id;
-        current_state_.pose.position.x = person.pose.position.x;
-        current_state_.pose.position.y = person.pose.position.y;
-        current_state_.twist.linear.x = person.twist.linear.x;
-        current_state_.twist.linear.y = person.twist.linear.y;
+        current_state.id = person.id;
+        current_state.pose.position.x = person.pose.position.x;
+        current_state.pose.position.y = person.pose.position.y;
+        current_state.twist.linear.x = person.twist.linear.x;
+        current_state.twist.linear.y = person.twist.linear.y;
 
-        ROS_INFO_STREAM("id is" << current_state_.id);  // デバック用
-        ROS_INFO_STREAM("position is x : " << current_state_.pose.position.x << "y : " << current_state_.pose.position.y);  // デバック用
-        ROS_INFO_STREAM("speed is x : " << current_state_.twist.linear.x << "y : " << current_state_.twist.linear.y);  // デバック用
+        current_ped_pose.position.x = current_state.pose.position.x;
+        current_ped_pose.position.y = current_state.pose.position.y;
+
+        current_ped_poses.poses.push_back(current_ped_pose);
+
+        if(current_state.id == 1)
+        {
+            // ROS_INFO_STREAM("id is" << current_state.id);  // デバック用
+            // ROS_INFO_STREAM("position is x : " << current_state.pose.position.x << "  y : " << current_state.pose.position.y);  // デバック用
+            // ROS_INFO_STREAM("speed is x : " << current_state.twist.linear.x << "  y : " << current_state.twist.linear.y);  // デバック用
+        }
     }
+
+    pub_current_ped_position_.publish(current_ped_poses);
+    ped_states_.pop();
 }
 
 //メイン文で実行する関数
@@ -44,7 +73,7 @@ void CostMapCreator::process()
 
     while(ros::ok())
     {
-        if(flag_ped_states_ == true)
+        if((flag_ped_states_ == true) && (flag_robot_odom_ == true))
         {
             ROS_INFO_STREAM("get ped_states!");  // デバック用
             id_veiwer();
