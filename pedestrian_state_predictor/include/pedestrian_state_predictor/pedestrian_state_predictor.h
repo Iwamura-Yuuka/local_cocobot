efe7d53a93936d864f133c5102e823e645a6bedf
Iwamura-Yuuka <ee203071@meiji.ac.jp>
2023-12-16 19:12:22 +0900

    fix ped_status frame

--- pedestrian_state_predictor/include/pedestrian_state_predictor/pedestrian_state_predictor.h
+++ pedestrian_state_predictor/include/pedestrian_state_predictor/pedestrian_state_predictor.h
@@ -5,7 +5,11 @@
 #include <queue>
 #include <geometry_msgs/PoseArray.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2/utils.h>
 
 // original_msgs
 #include <pedsim_msgs/AgentState.h>
@@ -25,32 +29,39 @@ private:
     void robot_odom_callback(const nav_msgs::Odometry::ConstPtr& msg);
 
     // 引数あり関数
-    void get_ped_data(pedestrian_msgs::PeopleStates& current_people, ros::Time now);                                                                   // 歩行者データを取得
-    double calc_distance(const double robot_x, const double robot_y, const double person_x, const double person_y);                                    // ロボットと歩行者の間の距離を計算
-    double calc_direction(const double robot_x, const double robot_y, const double person_x, const double person_y);                                   // 方位を計算
-    double normalize_angle(double theta);                                                                                                              // 適切な角度(-M_PI ~ M_PI)を返す
-    void predict_future_ped_states(const pedestrian_msgs::PeopleStates& current_people, pedestrian_msgs::PeopleStates& future_people, ros::Time now);  // 歩行者の将来位置を予測
-    void visualize_people_pose(const pedestrian_msgs::PeopleStates& people, const ros::Publisher& pub_people_poses, ros::Time now);                    // 歩行者の位置情報を可視化
+    void get_ped_data(pedestrian_msgs::PeopleStates& current_people, ros::Time now);                                                                                                                           // 歩行者データを取得
+    double calc_distance(const double robot_x, const double robot_y, const double person_x, const double person_y);                                                                                            // ロボットと歩行者の間の距離を計算
+    void transform_and_calc_speed(const pedestrian_msgs::PersonState& current_person, pedestrian_msgs::PersonState& selected_current_person, const double after_ones_x, const double after_ones_y);            // 歩行者情報をodomからbase_footprintに変更 & base_link座標系での速度を計算
+    double calc_direction(const double robot_x, const double robot_y, const double person_x, const double person_y);                                                                                           // 方位を計算
+    double normalize_angle(double theta);                                                                                                                                                                      // 適切な角度(-M_PI ~ M_PI)を返す
+    void transform_ped_pose(const double before_x, const double before_y,  pedestrian_msgs::PersonState& future_person);                                                                                       // 歩行者情報をodomからbase_footprintに変更
+    void predict_future_ped_states(const pedestrian_msgs::PeopleStates& current_people, pedestrian_msgs::PeopleStates& future_people, pedestrian_msgs::PeopleStates& selected_current_people, ros::Time now);  // 歩行者の将来位置を予測
+    void visualize_people_pose(const pedestrian_msgs::PeopleStates& people, const ros::Publisher& pub_people_poses, ros::Time now);                                                                            // 歩行者の位置情報を可視化
 
     // 引数なし関数
     void update_ped_state();  // 歩行者情報を更新
 
     // yamlファイルで設定可能な変数
-    int hz_;                               // ループ周波数 [Hz]
-    bool visualize_current_people_poses_;  // 現在の歩行者の位置を可視化するかの設定用
-    bool visualize_future_people_poses_;   // 予測した歩行者の将来位置を可視化するかの設定用
-    std::string robot_frame_;              // ロボットの位置情報のframe_id
-    std::string people_frame_;             // 歩行者の位置情報のframe_id
-    double predict_dist_border_;           // 歩行者の将来位置を予測する距離 [m]
-    double predict_time_resolution_;       // 1秒先を予測する距離 [m]
+    int hz_;                                        // ループ周波数 [Hz]
+    bool visualize_current_people_poses_;           // 歩行者の現在位置（odom）を可視化するかの設定用
+    bool visualize_selected_current_people_poses_;  // ロボットに近い歩行者の現在位置（base_footprint）を可視化するかの設定用
+    bool visualize_future_people_poses_;            // 予測した歩行者の将来位置を可視化するかの設定用
+    std::string sim_frame_;                         // シミュレーターからの歩行者の位置情報のframe_id
+    std::string robot_frame_;                       // ロボットの位置情報のframe_id
+    std::string people_frame_;                      // 歩行者の位置情報のframe_id
+    double predict_dist_border_;                    // 歩行者の将来位置を予測する距離 [m]
+    double predict_time_resolution_;                // 1秒先を予測する距離 [m]
 
     // その他の変数
-    double tmp_robot_x_;                   // 1ループ前のロボットのx座標格納用
-    double tmp_robot_y_;                   // 1ループ前のロボットのy座標格納用
+    double tmp_robot_x_;                            // 1ループ前のロボットのx座標格納用
+    double tmp_robot_y_;                            // 1ループ前のロボットのy座標格納用
     
     // msgの受け取り判定用
     bool flag_ped_states_ = false;
     bool flag_robot_odom_ = false;
+
+    // 座標変換の判定用
+    bool flag_frame_change_ = false;
 
     // NodeHandle
     ros::NodeHandle nh_;
@@ -62,9 +73,13 @@ private:
 
     // Publisher
     ros::Publisher pub_current_ped_poses_;
-    ros::Publisher pub_current_people_states_;
+    ros::Publisher pub_selected_current_ped_poses_;
+    ros::Publisher pub_selected_current_people_states_;
     ros::Publisher pub_future_ped_poses_;
     ros::Publisher pub_future_people_states_;
+
+    // tf
+    tf2_ros::Buffer tf_buffer_;
 
     // 各種オブジェクト
     std::queue<pedsim_msgs::AgentStatesConstPtr> ped_states_;  // 歩行者情報
--- pedestrian_state_predictor/src/pedestrian_state_predictor.cpp
+++ pedestrian_state_predictor/src/pedestrian_state_predictor.cpp
@@ -5,9 +5,11 @@ PedestrianStatePredictor::PedestrianStatePredictor():private_nh_("~")
     // param
     private_nh_.param("hz", hz_, {10});
     private_nh_.param("visualize_current_people_poses", visualize_current_people_poses_, {false});
+    private_nh_.param("visualize_selected_current_people_poses", visualize_selected_current_people_poses_, {false});
     private_nh_.param("visualize_future_people_poses", visualize_future_people_poses_, {false});
+    private_nh_.param("sim_frame", sim_frame_, {"odom"});
     private_nh_.param("robot_frame", robot_frame_, {"odom"});
-    private_nh_.param("people_frame", people_frame_, {"odom"});
+    private_nh_.param("people_frame", people_frame_, {"base_footprint"});
     private_nh_.param("predict_dist_border", predict_dist_border_, {8.0});
     private_nh_.param("predict_time_resolution", predict_time_resolution_, {2.5});
     private_nh_.param("tmp_robot_x", tmp_robot_x_, {0.0});
@@ -18,11 +20,12 @@ PedestrianStatePredictor::PedestrianStatePredictor():private_nh_("~")
     sub_robot_odom_ = nh_.subscribe("/robot_odom", 1, &PedestrianStatePredictor::robot_odom_callback, this);
 
     // publisher
-    pub_current_people_states_ = nh_.advertise<pedestrian_msgs::PeopleStates>("/current_people_states", 1);
+    pub_selected_current_people_states_ = nh_.advertise<pedestrian_msgs::PeopleStates>("/selected_current_people_states", 1);
     pub_future_people_states_ = nh_.advertise<pedestrian_msgs::PeopleStates>("/future_people_states", 1);
 
     // debug
     pub_current_ped_poses_ = nh_.advertise<geometry_msgs::PoseArray>("/current_ped_poses", 1);
+    pub_selected_current_ped_poses_ = nh_.advertise<geometry_msgs::PoseArray>("/selected_current_ped_poses", 1);
     pub_future_ped_poses_ = nh_.advertise<geometry_msgs::PoseArray>("/future_ped_poses", 1);
 
 }
@@ -47,7 +50,7 @@ void PedestrianStatePredictor::get_ped_data(pedestrian_msgs::PeopleStates& curre
     pedestrian_msgs::PersonState current_person;
 
     current_people.header.stamp = now;
-    current_people.header.frame_id = people_frame_;
+    current_people.header.frame_id = sim_frame_;
 
     // ped_states_の配列のうち，1回のpublish分のデータ（配列の先頭の要素）のみ取得
     const auto people_states = ped_states_.front();
@@ -63,8 +66,6 @@ void PedestrianStatePredictor::get_ped_data(pedestrian_msgs::PeopleStates& curre
 
         current_people.people_states.push_back(current_person);
     }
-
-    pub_current_people_states_.publish(current_people);
 }
 
 // ロボットと歩行者の間の距離を計算
@@ -74,6 +75,55 @@ double PedestrianStatePredictor::calc_distance(const double robot_x, const doubl
     const double dy = person_y - robot_y;
 
     return hypot(dx, dy);
+}
+
+// 歩行者情報をodomからbase_footprintに変更
+// base_link座標系での速度を計算
+void PedestrianStatePredictor::transform_and_calc_speed(const pedestrian_msgs::PersonState& current_person, pedestrian_msgs::PersonState& selected_current_person, const double after_ones_x, const double after_ones_y)
+{
+    geometry_msgs::TransformStamped tf;
+
+     // 歩行者情報をodomからbase_footprintに変更
+    try
+    {
+        tf = tf_buffer_.lookupTransform(people_frame_, sim_frame_, ros::Time(0));
+        flag_frame_change_ = true;
+
+        geometry_msgs::Pose person_pose_before;    // 座標変換前の歩行者位置格納用
+        geometry_msgs::Pose person_pose_after;     // 座標変換後の歩行者位置格納用
+
+        person_pose_before = current_person.pose;
+
+        // 歩行者の現在位置をdoTransformで座標変換
+        tf2::doTransform(person_pose_before, person_pose_after, tf);
+
+        selected_current_person.id = current_person.id;
+        selected_current_person.pose.position = person_pose_after.position;
+        selected_current_person.pose.orientation.x = 0.0;
+        selected_current_person.pose.orientation.y = 0.0;
+        selected_current_person.pose.orientation.z = 0.0;
+        selected_current_person.pose.orientation.w = 0.0;
+
+        geometry_msgs::Pose after_ones_person_pose_before;    // 座標変換前の1秒後の歩行者位置格納用
+        geometry_msgs::Pose after_ones_person_pose_after;     // 座標変換後の1秒後の歩行者位置格納用
+
+        after_ones_person_pose_before.position.x = after_ones_x;
+        after_ones_person_pose_before.position.y = after_ones_y;
+
+        // 1秒後の歩行者位置をdoTransformで座標変換
+        tf2::doTransform(after_ones_person_pose_before, after_ones_person_pose_after, tf);
+       
+        // base_footprint座標系における現在の歩行者位置と1秒後の歩行者位置の予測から，速度を計算
+        selected_current_person.twist.linear.x = after_ones_person_pose_after.position.x - selected_current_person.pose.position.x;
+        selected_current_person.twist.linear.y = after_ones_person_pose_after.position.y - selected_current_person.pose.position.y;
+
+    }
+    catch(tf2::TransformException& ex)
+    {
+        ROS_WARN("%s", ex.what());
+        flag_frame_change_ = false;
+        return;
+    }
 }
 
 // 方位を計算
@@ -95,13 +145,50 @@ double PedestrianStatePredictor::normalize_angle(double theta)
     return theta;
 }
 
+// 歩行者情報をodomからbase_footprintに変更
+void PedestrianStatePredictor::transform_ped_pose(const double before_x, const double before_y, pedestrian_msgs::PersonState& future_person)
+{
+    geometry_msgs::TransformStamped tf;
+
+    // 歩行者情報をodomからbase_footprintに変更
+    try
+    {
+        tf = tf_buffer_.lookupTransform(people_frame_, sim_frame_, ros::Time(0));
+        flag_frame_change_ = true;
+
+        geometry_msgs::Pose person_pose_before;    // 座標変換前の歩行者位置格納用
+        geometry_msgs::Pose person_pose_after;     // 座標変換後の歩行者位置格納用
+
+        person_pose_before.position.x = before_x;
+        person_pose_before.position.y = before_y;
+
+        // 歩行者の現在位置をdoTransformで座標変換
+        tf2::doTransform(person_pose_before, person_pose_after, tf);
+
+        future_person.pose.position = person_pose_after.position;
+        future_person.pose.orientation.x = 0.0;
+        future_person.pose.orientation.y = 0.0;
+        future_person.pose.orientation.z = 0.0;
+        future_person.pose.orientation.w = 0.0;
+    }
+    catch(tf2::TransformException& ex)
+    {
+        ROS_WARN("%s", ex.what());
+        flag_frame_change_ = false;
+        return;
+    }
+}
+
 // 歩行者の将来位置を予測
-void PedestrianStatePredictor::predict_future_ped_states(const pedestrian_msgs::PeopleStates& current_people, pedestrian_msgs::PeopleStates& future_people, ros::Time now)
+void PedestrianStatePredictor::predict_future_ped_states(const pedestrian_msgs::PeopleStates& current_people, pedestrian_msgs::PeopleStates& future_people, pedestrian_msgs::PeopleStates& selected_current_people, ros::Time now)
 {
     pedestrian_msgs::PersonState future_person;
+    pedestrian_msgs::PersonState selected_current_person;
 
     future_people.header.stamp = now;
     future_people.header.frame_id = people_frame_;
+    selected_current_people.header.stamp = now;
+    selected_current_people.header.frame_id = people_frame_;
 
     for(const auto& current_person : current_people.people_states)
     {
@@ -111,6 +198,20 @@ void PedestrianStatePredictor::predict_future_ped_states(const pedestrian_msgs::
         // ロボットからの距離が一定以下の歩行者に関して，将来位置を予測
         if(dist <= predict_dist_border_)
         {
+            // base_link座標系での速度計算のために1秒先の位置を予測
+            const double after_ones_x = current_person.pose.position.x + (current_person.twist.linear.x * 1.0);
+            const double after_ones_y = current_person.pose.position.y + (current_person.twist.linear.y * 1.0);
+            
+            // 歩行者の現在位置をbase_footprint座標系に変換
+            // base_link座標系での速度を計算
+            transform_and_calc_speed(current_person, selected_current_person, after_ones_x, after_ones_y);
+            selected_current_people.people_states.push_back(selected_current_person);
+
+            ROS_INFO_STREAM("----- selected_current -----");  // デバック用
+            ROS_INFO_STREAM("id : " << selected_current_person.id);  // デバック用
+            ROS_INFO_STREAM("x : " << selected_current_person.pose.position.x);  // デバック用
+            ROS_INFO_STREAM("y : " << selected_current_person.pose.position.y);  // デバック用
+
             // 何秒先の将来位置を予測するか
             const double predict_time = dist / predict_time_resolution_;
 
@@ -126,23 +227,33 @@ void PedestrianStatePredictor::predict_future_ped_states(const pedestrian_msgs::
 
             // ロボットに対する歩行者の将来位置の方位を計算
             const double theta = person_theta - robot_theta;
+            ROS_INFO_STREAM("theta : " << theta);  // デバック用
 
             // ロボットの進行方向前方に移動する歩行者のみ考慮
             if((theta <= M_PI/2) && (theta >= -M_PI/2))
             {
                 // 歩行者の予測データを格納
                 future_person.id = current_person.id;
-                future_person.pose.position.x = future_x;
-                future_person.pose.position.y = future_y;
-                future_person.twist.linear.x = current_person.twist.linear.x;
-                future_person.twist.linear.y = current_person.twist.linear.y;
+                transform_ped_pose(future_x, future_y, future_person);  //歩行者の将来位置をbase_footprint座標系に変換
+                future_person.twist.linear.x = selected_current_person.twist.linear.x;
+                future_person.twist.linear.y = selected_current_person.twist.linear.y;
+
+                ROS_INFO_STREAM("----- future -----");  // デバック用
+                ROS_INFO_STREAM("id : " << future_person.id);  // デバック用
+                ROS_INFO_STREAM("x : " << future_person.pose.position.x);  // デバック用
+                ROS_INFO_STREAM("y : " << future_person.pose.position.y);  // デバック用
 
                 future_people.people_states.push_back(future_person);
             }
         }
     }
 
-    pub_future_people_states_.publish(future_people);
+    if(flag_frame_change_ == true)
+    {
+        pub_selected_current_people_states_ .publish(selected_current_people);
+        pub_future_people_states_.publish(future_people);
+
+    }
 }
 
 // 歩行者の位置情報を可視化
@@ -151,13 +262,17 @@ void PedestrianStatePredictor::visualize_people_pose(const pedestrian_msgs::Peop
     geometry_msgs::Pose person_pose;        // 歩行者1人の位置情報
     geometry_msgs::PoseArray people_poses;  // 全歩行者の位置情報
     people_poses.header.stamp = now;
-    people_poses.header.frame_id  = people_frame_;
+    people_poses.header.frame_id  = people.header.frame_id;
 
     // msg型を pedestrian_msgs/PeopleStates から geometry_msgs/PoseArray に変更
     for(const auto& person : people.people_states)
     {
         person_pose.position.x = person.pose.position.x;
         person_pose.position.y = person.pose.position.y;
+        person_pose.orientation.x = person.pose.orientation.x;
+        person_pose.orientation.y = person.pose.orientation.y;
+        person_pose.orientation.z = person.pose.orientation.z;
+        person_pose.orientation.w = person.pose.orientation.w;
 
         people_poses.poses.push_back(person_pose);
     }
@@ -170,21 +285,28 @@ void PedestrianStatePredictor::update_ped_state()
 {
     pedestrian_msgs::PeopleStates current_people;
     pedestrian_msgs::PeopleStates future_people;
+    pedestrian_msgs::PeopleStates selected_current_people;
     ros::Time now = ros::Time::now();
 
     // 歩行者データを取得
     get_ped_data(current_people, now);
 
-    // 歩行者の現在位置の可視化
+    // 歩行者の現在位置（odom）の可視化
     if(visualize_current_people_poses_)
         visualize_people_pose(current_people, pub_current_ped_poses_, now);
 
     // 歩行者の将来位置を予測
-    predict_future_ped_states(current_people, future_people, now);
+    predict_future_ped_states(current_people, future_people, selected_current_people, now);
 
-    // 歩行者の将来位置（予測）の可視化
-    if(visualize_future_people_poses_)
-        visualize_people_pose(future_people, pub_future_ped_poses_, now);
+    if(flag_frame_change_ == true)
+    {
+        // ロボットに近い歩行者の現在位置（base_footprint）の可視化
+        if(visualize_selected_current_people_poses_)
+            visualize_people_pose(selected_current_people, pub_selected_current_ped_poses_, now);
+        // 歩行者の将来位置（予測）の可視化
+        if(visualize_future_people_poses_)
+            visualize_people_pose(future_people, pub_future_ped_poses_, now);
+    }
 
     // ped_states_の配列のうち取得済みのデータ（配列の先頭の要素）を削除
     // これをしないと，front() でデータを取得する際，同じデータしか取得できない
@@ -199,6 +321,7 @@ void PedestrianStatePredictor::update_ped_state()
 void PedestrianStatePredictor::process()
 {
     ros::Rate loop_rate(hz_);
+    tf2_ros::TransformListener tf_listener(tf_buffer_);
 
     while(ros::ok())
     {
