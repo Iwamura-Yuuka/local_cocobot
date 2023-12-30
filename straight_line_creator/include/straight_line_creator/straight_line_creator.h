85ac7b5c2b5ce0a7cc077e82b6d95041af6990c5
Iwamura-Yuuka <ee203071@meiji.ac.jp>
2023-12-30 19:04:21 +0900

    fix

--- straight_line_creator/include/straight_line_creator/straight_line_creator.h
+++ straight_line_creator/include/straight_line_creator/straight_line_creator.h
@@ -12,9 +12,6 @@ public:
     void process();
 
 private:
-    // 引数あり関数
-    // void visualize_point(const double x, const double y, const ros::Publisher& pub_point, ros::Time now);
-
     // 引数なし関数
     void create_path();
 
@@ -23,8 +20,6 @@ private:
     std::string path_frame_;  // 生成するpathのframe_id
     double init_x_;          // スタート地点のx座標
     double init_y_;          // スタート地点のy座標
-    // double goal_x_;           // ゴール地点のx座標
-    // double goal_y_;           // ゴール地点のy座標
     double init_theta_;       // 生成する軌道の向き
     double cource_length_;       // 目標軌道の長さの最大値 [m]
     double resolution_;              // 軌道生成時の刻み幅 [m]
@@ -37,13 +32,9 @@ private:
 
     // Publisher
     ros::Publisher pub_global_path_;
-    // ros::Publisher pub_start_point_;
-    // ros::Publisher pub_goal_point_;
 
     // 各種オブジェクト
     nav_msgs::Path global_path_;  // global path
-    // geometry_msgs::PoseStamped start_point_;  // スタート地点
-    // geometry_msgs::PoseStamped goal_point_;   // ゴール地点
 };
 
 #endif // STRAIGHT_LINE_CREATOR_H
--- straight_line_creator/src/straight_line_creator.cpp
+++ straight_line_creator/src/straight_line_creator.cpp
@@ -7,18 +7,12 @@ StraightLineCreator::StraightLineCreator():private_nh_("~")
     private_nh_.param("path_frame", path_frame_, {"odom"});
     private_nh_.param("init_x", init_x_, {0.0});
     private_nh_.param("init_y", init_y_, {0.0});
-    // private_nh_.param("goal_x", goal_x_, {5.0});
-    // private_nh_.param("goal_x", goal_x_, {20.0});
     private_nh_.param("init_theta", init_theta_, {0.0});
     private_nh_.param("cource_length", cource_length_, {20.0});
     private_nh_.param("resolution", resolution_, {0.05});
 
     // publisher
     pub_global_path_ = nh_.advertise<nav_msgs::Path>("/global_path", 1);
-
-    // debug
-    // pub_start_point_ = nh_.advertise<geometry_msgs::PoseStamped>("/start_point", 1);
-    // pub_goal_point_ = nh_.advertise<geometry_msgs::PoseStamped>("/goal_point", 1);
 }
 
 // 軌道を生成
